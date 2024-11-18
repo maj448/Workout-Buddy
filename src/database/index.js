343432affdae1d3a79db8c0a1eac170c64399d9b
maj448 <joycemarissa20@gmail.com>
2024-11-18 00:28:06 -0600

    saving before trying realm again

--- src/database/index.js
+++ src/database/index.js
@@ -14,10 +14,8 @@ import Workout from '../model/Workouts'
 // First, create the adapter to the underlying database:
 const adapter = new SQLiteAdapter({
   schema: mySchema,
-  // (You might want to comment it out for development purposes -- see Migrations documentation)
   migrations,
-  // (optional database name or file system path)
-  // dbName: 'myapp',
+  dbName: 'workoutBuddyDb',
   // (recommended option, should work flawlessly out of the box on iOS. On Android,
   // additional installation steps have to be taken - disable if you run into issues...)
   //jsi: true, /* Platform.OS === 'ios' */
